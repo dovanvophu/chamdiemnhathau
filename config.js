15417fe91ee46fec42d4b4696862e73705144a50
dovanvophu <dovanvophu@gmail.com>
2026-08-27 16:41:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbx6VkaZQEncY_HCixoG-oH8x9ihkE_ZFtr_FtLggKwUsL371ylIckP2kp1cPki9rkNP/exec'
 };
