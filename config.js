8882356921d2535e195a0372569a557a1ceecfcc
dovanvophu <dovanvophu@gmail.com>
2026-09-04 13:35:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyLJH2pyse3DAME4ML2Uu9_p2RNTmBt1mGWwrHTmOgxLz6XS11JBLa-VZmDcQwIYV5c/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyuviC0iqGufPKHCHEBguUn2VKYp1AA0dW5YwytfNybWUwlF2MLckClFyWWX4XGwwGU/exec'
 };
